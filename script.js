4c4b8588d4d4b88e9d425f9c7a0577ac33b04218
a-ziz-7 <aziz.abdusamiev7@gmail.com>
2024-03-23 21:09:21 -0400

    The general logic is done

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ var questions = [];
 answer_id = "a";
 correct_answer = "a";
 fun_fact = "";
+turns = 6;
+correct = 0;
 let turn = 0; // Initialize the turn counter
 let usedIndices = [];
 
@@ -11,7 +13,6 @@ fetch('data.json')
   .then(response => response.json())
   .then(data => {
     questions = data.questions; 
-    // console.log(questions);
     gameLoop();
   })
   .catch(error => {
@@ -23,16 +24,14 @@ function generateQuestion() {
 	document.getElementById('question').textContent = data.question;
 	
 	const answersContainer = document.getElementById('ans');
-	answersContainer.innerHTML = ''; // Clear the answers container before adding new answers
+	answersContainer.innerHTML = '';
 	answersContainer.id = 'ans';
-	// console.log(data.options);
 	for (let key in data.options) {
 		const answerDiv = document.createElement('div');
 		answerDiv.textContent = key + ") " + data.options[key];
 		answerDiv.className = 'answer';
 		answerDiv.id = ""+key;
 		answerDiv.addEventListener('click', function() {
-			// console.log('Selected answer:', this.textContent);
 			answer_id = this.id;
 			correct_answer = data.correct_answer;
 			fun_fact = data.fun_fact;
@@ -40,7 +39,7 @@ function generateQuestion() {
 			const allAnswers = document.querySelectorAll('.answer');
 			allAnswers.forEach(answer => {
 				if (answer !== this) {
-					answer.style.backgroundColor = '#e0e0e0'; // Reset background color to default
+					answer.style.backgroundColor = '#e0e0e0';
 				}
   			});
 			
@@ -49,10 +48,9 @@ function generateQuestion() {
 	}
 }
 
-// Function to handle game loop
 function gameLoop() {
   console.log('Game loop started');
-  generateQuestion(); // Generate the first question for testing
+  generateQuestion(); 
 }
 
 function button() {
@@ -66,6 +64,7 @@ function button() {
 		if (answer_id === correct_answer) {
 			console.log('Correct answer!');
 			document.getElementById(answer_id).style.backgroundColor = '#7FFF00';
+			correct++;
 		} else {
 			console.log('Incorrect answer!');
 			document.getElementById(answer_id).style.backgroundColor = 'red';
@@ -80,25 +79,23 @@ function button() {
 	} else {
 		console.log('Next question!');
 		document.getElementById('fact').textContent = '';
-		// const allAnswers = document.querySelectorAll('.answer');
-		// console.log(allAnswers);
-		// allAnswers.forEach(answer => {
-		// 	answer.classList.remove('unselectable');
-		// });
-
+		turn++;
+		if (turn == turns) {
+			alert('Game Over!\nCongratulations on completing the quiz!\nYou got ' + correct + ' out of ' + turns + ' questions correct.\nKeep learning and exploring the wonders of our natural world!');
+			window.location.href = 'index.html';
+			return;
+		}
 		b.textContent = 'Submit';
 		generateQuestion(questions[0]);
 	}
 }
 
 function generateUniqueRandomIndex() {
-    let index = Math.floor(Math.random() * questions.length); // Generate a random index within the range of the questions array
-
-    // Check if the index has already been used
+    let index = Math.floor(Math.random() * questions.length);
     while (usedIndices.includes(index)) {
-        index = Math.floor(Math.random() * questions.length); // Regenerate the index
+        index = Math.floor(Math.random() * questions.length);
     }
 	answer_id = "e";
-    usedIndices.push(index); // Add the index to the list of used indices
+    usedIndices.push(index);
     return index;
 }
